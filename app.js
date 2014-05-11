b96ccc3214148509f679824cd01e84ab3865e8d0
Shane Curran <s@velodro.me>
2014-05-11 15:27:23 +0100

    Included modules + barebones app

---
+++ app.js
@@ -0,0 +1,18 @@
+//
+// Crippl.es
+//
+// @author Shane Curran
+// @author Rory Hughes
+//
+// @git https://github.com/narruc/Crippl.es
+//
+
+// Include all the necessary libraries etc.
+var express   = require("express");
+var app       = express();
+var jade      = require("jade");
+var sequelize = require("sequelize");
+
+var db        = new sequelize("cripples", {
+  dialect: "sqlite"
+});
